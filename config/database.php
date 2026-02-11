5642e81e069142da595543e7ff518d68bd56d7b8
unknown <darenmarcdavidcuesta@yahoo.com>
2026-02-11 09:45:34 +0800

    Add TiDB CA file and SSL config

--- config/database.php
+++ config/database.php
@@ -45,21 +45,21 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-    'host' => env('DB_HOST'),
-    'port' => env('DB_PORT', '4000'),
-    'database' => env('DB_DATABASE', 'forge'),
-    'username' => env('DB_USERNAME', 'forge'),
-    'password' => env('DB_PASSWORD', ''),
-    'unix_socket' => env('DB_SOCKET', ''),
-    'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
-    'prefix' => '',
-    'prefix_indexes' => true,
-    'strict' => true,
-    'engine' => null,
-    'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-    ]) : [],
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT', '4000'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'mariadb' => [
@@ -78,10 +78,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-    defined('PDO::MYSQL_ATTR_SSL_CA') 
-        ? \PDO::MYSQL_ATTR_SSL_CA 
-        : (\Pdo\Mysql\Driver::ATTR_SSL_CA ?? null) => env('MYSQL_ATTR_SSL_CA'),
-]) : [],
+                PDO::MYSQL_ATTR_SSL_CA => base_path(env('MYSQL_ATTR_SSL_CA', '')),
+            ]) : [],
         ],
 
         'pgsql' => [
@@ -149,7 +147,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
